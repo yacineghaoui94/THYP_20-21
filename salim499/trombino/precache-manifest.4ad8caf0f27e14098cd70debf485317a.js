0fced53cd24988e192cae886b64e367d44066d48
salim499 <salimhassouna842@gmail.com>
2020-10-07 00:34:24 +0200

    second commit

---
+++ salim499/trombino/precache-manifest.4ad8caf0f27e14098cd70debf485317a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6276c0549e327cd1444eee8a4d1ea828",
+    "url": "/hossine/index.html"
+  },
+  {
+    "revision": "5a48a53c825220c5a133",
+    "url": "/hossine/static/css/main.b99e7ae4.chunk.css"
+  },
+  {
+    "revision": "e6e647ae0fa20f9ab503",
+    "url": "/hossine/static/js/2.53328276.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hossine/static/js/2.53328276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a48a53c825220c5a133",
+    "url": "/hossine/static/js/main.04fc7635.chunk.js"
+  },
+  {
+    "revision": "05f654e2d42795f8c68a",
+    "url": "/hossine/static/js/runtime-main.1cf8f86e.js"
+  }
+]);
