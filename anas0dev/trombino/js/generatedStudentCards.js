8d4efcefb5c6ae7eb2457b599366b127a3d57a70
Unknown <b.abdelrahim@outlook.fr>
2020-10-11 15:58:31 +0200

    Adding the filter students by course "parcours"

--- anas0dev/trombino/js/generatedStudentCards.js
+++ anas0dev/trombino/js/generatedStudentCards.js
@@ -10,6 +10,7 @@ let facet = {};
 let studCards = document.getElementById("studCards");
 let searchButton = document.getElementById("searchButton");
 let searchInput = document.getElementById("searchInput");
+let navbarFacet = document.getElementById("navbarFacet");
 
 
 let file_csv = "./data/data.csv";
@@ -32,29 +33,6 @@ function getUrl(url){
     return urlTof;
 }
 
-// function createDiv(className, style){
-//     let newElt = document.createElement("div");
-//     newElt.className = className;
-//     newElt.style = style;
-//     return newElt;
-// }
-
-function createBalise(balise, className, src, style){
-    let newElt = document.createElement(balise);
-    newElt.className = className;
-    newElt.src = src;
-    newElt.style = style;
-    return newElt;
-}
-
-// function createImg(src, className, style){
-//     let newEltImg = document.createElement("IMG");
-//     newEltImg.src = src;
-//     newEltImg.className = className;
-//     newEltImg.style = style;
-//     return newEltImg;
-// }
-
 function setData(data){
     dataCsv = data;
     dataCsv.forEach(function(etud, id) {
@@ -66,6 +44,7 @@ function setData(data){
 
     studListDisplay = studListAll;
     generateCards(studListDisplay);
+    createNavFacet(facet);
     // studListDisplay.forEach(stud => {
     //     studCards.appendChild(createCards_1(stud));
     // });
@@ -91,9 +70,12 @@ function getUrl(url){
 
 function createBalise(balise, className, src, style){
     let newElt = document.createElement(balise);
-    newElt.className = className;
-    newElt.src = src;
-    newElt.style = style;
+    if(className)
+        newElt.className = className;
+    if(src)
+        newElt.src = src;
+    if(style)
+        newElt.style = style;
     return newElt;
 }
 
@@ -147,7 +129,6 @@ function filterStudByName(){
 
     for(let i = 0; i < studName.length; i++){
         if(studName[i].textContent.toLowerCase().indexOf(nameSearched) == -1){
-            console.log(cards[i].style.display);
             cards[i].style.display = "none";
         }else{
             cards[i].style.display = "";
@@ -155,8 +136,102 @@ function filterStudByName(){
     }
 }
 
+
 searchButton.onclick = filterStudByName;
 searchInput.addEventListener("keyup", e => {
         filterStudByName();
 });
 
+
+function createNavFacet(facet) {
+    let newNavbar = createBalise("ul", "navbar-nav mr-auto");
+    let parcours = createDropdown("Parcours", facet);
+    newNavbar.appendChild(parcours);
+
+    // let newNavbar2 = createBalise("ul", "navbar-nav mr-auto");
+    // let ordinateur = createDropdown("Ordinateur", facet);
+    // newNavbar2.appendChild(ordinateur);
+    // console.log(facet);
+
+    // navbarFacet.prepend(newNavbar2);
+    navbarFacet.prepend(newNavbar);
+}
+
+function createDropdown(name, facet) {
+    if(facet[name]){
+        let newDropdown = createBalise("li", "nav-item dropdown");
+        // console.log(facet[name]);
+        let title = createBalise("a", "nav-link dropdown-toggle");
+        title.href = "#";
+        title.id = "navbarDropdown";
+        // title.role = "button";
+        title.setAttribute("role", "button");
+        title.setAttribute("data-toggle", "dropdown")
+        title.setAttribute("aria-haspopup", "true")
+        title.setAttribute("aria-expanded", "false")
+        // title["data-toggle"] = "dropdown";
+        // title["aria-haspopup"] = "true";
+        // title["aria-expanded"] = "false";
+        title.appendChild(document.createTextNode(name));
+        newDropdown.appendChild(title);
+
+        let subTitles = createBalise("div", "dropdown-menu");
+        subTitles.setAttribute("aria-labelledby", "navbarDropdown");
+        // subTitles["aria-labelledby"] = "navbarDropdown";
+
+        for(const exp in facet[name].facetList){
+            let subTitle = createBalise("a", "dropdown-item");
+            subTitle.href = "#";
+            // console.log(exp);
+            let checkbox = createBalise("div", "form-check");
+            let checkInput = createBalise("input", "form-check-input");
+            checkInput.setAttribute("type", "checkbox");
+            checkInput.setAttribute("value", "");
+            // checkInput.type = "checkbox";
+            // checkInput.value = "";
+            checkInput.id = "defaultCheck1";
+            checkbox.appendChild(checkInput);
+            let checkLabel = createBalise("label", "form-check-label");
+            checkLabel.setAttribute("for", "defaultCheck1");
+            // checkLabel.for = "defaultCheck1";
+            checkLabel.appendChild(document.createTextNode(exp));
+            checkbox.appendChild(checkLabel);
+
+            subTitle.appendChild(checkbox);
+            subTitles.appendChild(subTitle);
+        }
+        newDropdown.appendChild(title);
+        newDropdown.appendChild(subTitles);
+        
+
+        return newDropdown;
+    }
+}
+
+{/* <ul class="navbar-nav mr-auto">			
+			<li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Parcours
+			  </a>
+			  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<a class="dropdown-item" href="#">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+						<label class="form-check-label" for="defaultCheck1">
+						  Default checkbox
+						</label>
+					  </div>
+				</a>
+				<a class="dropdown-item" href="#">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+						<label class="form-check-label" for="defaultCheck1">
+						  Default checkbox
+						</label>
+					  </div>
+				</a>
+				<div class="dropdown-divider"></div>
+				<a class="dropdown-item" href="#">Something else here</a>
+			  </div>
+			</li>
+		</ul> */}
